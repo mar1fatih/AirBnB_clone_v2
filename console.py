0cc27e4fdf772c75220b652342fa003b6a1508ab
mar1fatih <marouanefatih631@gmail.com>
2024-01-17 12:30:33 -0800

    console

--- console.py
+++ console.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """ Console Module """
-import cmd
 import sys
+import cmd
 from models.base_model import BaseModel
 from models.__init__ import storage
-from models.user import User
 from models.place import Place
+from models.user import User
 from models.state import State
+from models.review import Review
 from models.city import City
 from models.amenity import Amenity
-from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Contains the functionality for the HBNB console"""
-
-    # determines prompt for interactive/non-interactive modes
-    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+    """ contains HBNB console """
 
     classes = {
                'BaseModel': BaseModel, 'User': User, 'Place': Place,
@@ -29,56 +26,42 @@ class HBNBCommand(cmd.Cmd):
              'max_guest': int, 'price_by_night': int,
              'latitude': float, 'longitude': float
             }
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     def preloop(self):
-        """Prints if isatty is false"""
+        """ Prints if isatty is false """
         if not sys.__stdin__.isatty():
             print('(hbnb)')
 
     def precmd(self, line):
-        """Reformat command line for advanced command syntax.
+        """ Reformat command line for advanced command syntax.
 
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
-        (Brackets denote optional fields in usage example.)
-        """
-        _cmd = _cls = _id = _args = ''  # initialize line elements
+        (Brackets denote optional fields in usage example.)"""
+        _cmd = _cls = _id = _args = ''
 
-        # scan for general formating - i.e '.', '(', ')'
         if not ('.' in line and '(' in line and ')' in line):
             return line
 
-        try:  # parse line left to right
-            pline = line[:]  # parsed line
+        try:
+            pline = line[:]
 
-            # isolate <class name>
             _cls = pline[:pline.find('.')]
-
-            # isolate and validate <command>
             _cmd = pline[pline.find('.') + 1:pline.find('(')]
             if _cmd not in HBNBCommand.dot_cmds:
                 raise Exception
 
-            # if parantheses contain arguments, parse them
             pline = pline[pline.find('(') + 1:pline.find(')')]
             if pline:
-                # partition args: (<id>, [<delim>], [<*args>])
-                pline = pline.partition(', ')  # pline convert to tuple
-
-                # isolate _id, stripping quotes
+                pline = pline.partition(', ')
                 _id = pline[0].replace('\"', '')
-                # possible bug here:
-                # empty quotes register as empty _id when replaced
-
-                # if arguments exist beyond _id
-                pline = pline[2].strip()  # pline is now str
+                pline = pline[2].strip()
                 if pline:
-                    # check for *args or **kwargs
                     if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
-                        # _args = _args.replace('\"', '')
             line = ' '.join([_cmd, _cls, _id, _args])
 
         except Exception as mess:
@@ -114,14 +97,14 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class """
+        """ Create objects of a class """
         if not args:
             print("** class name missing **")
             return
         try:
-            arguments = args.split(" ")
-            cmd = eval("{:s}()".format(arguments[0]))
-            for prms in arguments[1:]:
+            argument = args.split(" ")
+            cmd = eval("{:s}()".format(argument[0]))
+            for prms in argument[1:]:
                 if "=" not in prms:
                     continue
                 key, value = prms.split("=")
