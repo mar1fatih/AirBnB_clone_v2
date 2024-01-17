0e5aa003c079430e249e044bf20d16405703db7b
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-01-17 13:16:17 -0800

    tasks

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" """
+""" Test module for City """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """ Test class for City model """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initializes test_City instance """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """ Test the 'state_id' attribute """
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """ Test the 'name' attribute """
         new = self.value()
         self.assertEqual(type(new.name), str)
