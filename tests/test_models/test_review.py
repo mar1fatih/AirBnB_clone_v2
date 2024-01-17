ecc884e7b831af87d659670b29a59d053cdbb557
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-01-17 13:19:19 -0800

    tasks

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" """
+""" Test module for Review """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """ Test class for Review model """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initializes test_Review instance """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ Test the 'place_id' attribute """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ Test the 'user_id' attribute """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ Test the 'text' attribute """
         new = self.value()
         self.assertEqual(type(new.text), str)
