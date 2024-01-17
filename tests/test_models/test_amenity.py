83bdd343f088211c6a87b43a7db8871661703c52
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-01-17 12:52:54 -0800

    tasks

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" Test module for Amenity """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ Test class for Amenity model """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initializes test_Amenity instance """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """ Test the 'name' attribute """
         new = self.value()
         self.assertEqual(type(new.name), str)
