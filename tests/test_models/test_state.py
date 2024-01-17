dbd4a854c1f119af2fe7f96f88fb439f13f42f22
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-01-17 13:21:07 -0800

    tasks

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" Test module for State """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ Test class for State model """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initializes test_State instance """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """ Test the 'name' attribute """
         new = self.value()
         self.assertEqual(type(new.name), str)
