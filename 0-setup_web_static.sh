2f8ca5631ef57c6688981dc308ec217b2ea86d45
mar1fatih <marouanefatih631@gmail.com>
2024-02-01 08:33:26 -0800

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 # set up the web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 sudo mkdir /data
 sudo mkdir -p /data/web_static
 sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
