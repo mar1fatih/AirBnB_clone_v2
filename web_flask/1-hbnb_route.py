f9a5cfdf14e81a7c9e7752ca3642ed7d73719e5d
mar1fatih <marouanefatih631@gmail.com>
2024-03-23 16:06:35 -0700

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""/hbnb: display “HBNB”"""
+from flsk import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    """returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb_page():
+    """returns HBNB"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
