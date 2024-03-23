caafe8832b51b0394275f490fa4a79b3500fd6b6
mar1fatih <marouanefatih631@gmail.com>
2024-03-23 16:28:40 -0700

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,6 +22,7 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>/", strict_slashes=False)
 def pythoniscool(text='is_cool'):
     """display python followed by text variable"""
